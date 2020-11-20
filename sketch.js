c706b0412cd8c4cf5e69e72ab8db41d597a37d7e
ChiluveruTejasree <chiluverutejasree@gmail.com>
2020-11-20 20:17:32 +0530

    Platform Added

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-
+var platform;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -16,7 +16,8 @@ function setup(){
     world = engine.world;
 
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(300,200,150,70);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
